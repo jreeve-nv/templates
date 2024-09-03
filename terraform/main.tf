dc452de8a3a34aebd2278d8a99e5debe8ecbc4ab
jreeve-nv <jennifer.reeve@netvalue.nz>
2024-09-03 13:02:04 +1200

    adding terraform configuration files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
 # from Hashicorp's getting started with AWS tutorial
+# Terraform settings
 terraform {
-  required_providers {
+  required_providers { # find providers here: https://registry.terraform.io/browse/providers
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
@@ -10,15 +11,21 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
+# Provider specific settings
+
+provider "aws" { # configures the specified provider
+  profile = "default"
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
+# Resource definitions
+## can be physical/virtual component (ie instance) or logical resource (ie application)
+resource "aws_instance" "app_server" { # resource blocks are named by combining the resource type and resource name specified here, this resource will be named aws_instance.app_server
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    # Name = "ExampleAppServerInstance"
+    Name = var.instance_name # can now provide a -var "Name" to the apply command and customize the resource name
   }
 }
